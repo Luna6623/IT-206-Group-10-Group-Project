a0c8f65398074b83b98e8ed091c27a5d79405b57
Nathaniel Sykes <lunsykes06@gmail.com>
2026-04-29 11:46:30 -0400

    Update ClinicImp.java

--- ClinicImp.java
+++ ClinicImp.java
@@ -2,6 +2,49 @@ public class ClinicImp {
     
     public static void main(String[] args) {
         // Nathaniel Sykes
+        // Department and Patient Arrays
+        Department[] deptArray = new Department[3];
+        Patient[] patientArray = new Patient[Patient.getPatientNum()];
+
+        // Variables for do while loop
+        int choice = 0;
+        boolean mainBool = false;
+
+        do {
+            try {
+                choice = Integer.parseInt(JOptionPane.showInputDialog("Enter your choice (1-6)\n1. Add patient\n2. Delete Patient\n3. Search Patient\n4. Display Patient Records\n5. Display Menu\n6. Exit"));
+                switch (choice) {
+                    case 1:
+                        addPatient();
+                        break;
+                    case 2:
+                        String deleteName = JOptionPane.showInputDialog("Enter the patient's name.");
+                        String deleteAddress = JOptionPane.showInputDialog("Enter the patient's address.");
+                        deletePatient(patientArray, deleteName, deleteAddress);
+                        break;
+                    case 3:
+                        String searchName = JOptionPane.showInputDialog("Enter the patient's name.");
+                        String searchAddress = JOptionPane.showInputDialog("Enter the patient's address.");
+                        searchPatient(searchName.toLowerCase(), searchAddress.toLowerCase());
+                        break;
+                    case 4:
+                        String displayName = JOptionPane.showInputDialog("Enter the patient's name.");
+                        String displayAddress = JOptionPane.showInputDialog("Enter the patient's address.");
+                        Patient displayPatient = searchPatient(displayName, displayAddress);
+                        displayPatientRecord(displayPatient);
+                        break;
+                    case 5:
+                        displayMenu();
+                        break;
+                    case 6:
+                        JOptionPane.showMessageDialog(null, "Thank you for using this app!");
+                        mainBool = true;
+                        break;
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error! You must select one of the options.");
+            }
+        } while (choice > -1 && choice <= 6 && !mainBool);
     }
 
     public static void addPatient() {
@@ -10,6 +53,36 @@ public class ClinicImp {
 
     public static void deletePatient() {
         // Nathaniel Sykes
+
+        try {
+            int index = -1;
+            for (int i = 0; i < Patient.getPatientNum(); i++) {
+                if ((patientArray[i].getPatientName().equalsIgnoreCase(deleteName)) && (patientArray[i].getPatientAddress().equalsIgnoreCase(deleteAddress))) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1) {
+                throw new IllegalArgumentException("Sorry, that patient is not in the database.");
+            }
+
+            boolean deleteBool = ConfirmDeletion();
+            if (deleteBool != true) {
+                throw new IllegalArgumentException("Thank you for trying our deletion service!");
+            }
+
+            for (int i = index; i < Patient.getPatientNum(); i++) {
+                patientArray[i] = patientArray[i+1];
+            }
+
+            patientArray[Patient.getPatientNum()] = null;
+
+            Patient.updatePatientNum();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
     }
 
     public static Patient searchPatient() {
@@ -18,6 +91,10 @@ public class ClinicImp {
 
     public static void displayPatientRecord() {
         // Nathaniel Sykes
+        String output = "** Patient Record **\n\n";
+        output += displayPatient.toString();
+
+        JOptionPane.showMessageDialog(null, output);
     }
 
     public static void displayMenu() {
@@ -26,6 +103,29 @@ public class ClinicImp {
 
     public static boolean ConfirmDeletion() {
         // Nathaniel Sykes
+
+        int confirmInput = 0;
+        boolean switchBool = false;
+        boolean confirmBool;
+        do { 
+            try {
+                confirmInput = Integer.parseInt(JOptionPane.showInputDialog("Enter your choice to confirm your deletion of this patient\n1 for Yes\n2 for No"));
+                switch (confirmInput) {
+                    case 1:
+                        confirmBool = true;
+                        switchBool = true;
+                        break;
+                    case 2:
+                        confirmBool = false;
+                        switchBool = true;
+                        break;
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error! You must select one of the options.");
+            }
+        } while (!switchBool && confirmInput > -1 && confirmInput <= 2);
+
+        return confirmBool;
     }
 
 }
